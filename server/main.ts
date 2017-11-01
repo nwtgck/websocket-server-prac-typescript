6555f04f1085342940031f2978d819ce4d7b510b
Ryo Ota <nwtgck@gmail.com>
2017-11-01 12:29:02 +0900

    [change] Create "hello, world" http server in server/main

--- server/main.ts
+++ server/main.ts
@@ -1,1 +1,10 @@
-console.log('hello, world')
+import * as http from 'http'
+
+
+const server: http.Server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
+    res.end('<h1>hello, world</h1>')
+});
+
+server.listen(8080);
+
+console.log('hello, world');
