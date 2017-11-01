d5df887d54701a6c27748110123863f73546bbd7
Ryo Ota <nwtgck@gmail.com>
2017-11-01 13:04:27 +0900

    [change] Add simple WebSocket server practice

--- server/main.ts
+++ server/main.ts
@@ -1,10 +1,29 @@
-import * as http from 'http'
+// (from: https://medium.com/factory-mind/websocket-node-js-express-step-by-step-using-typescript-725114ad5fe4)
+import * as express   from 'express';
+import * as http      from 'http';
+import * as WebSocket from 'ws';
 
+const HTTP_PORT = 8080;
 
-const server: http.Server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
-    res.end('<h1>hello, world</h1>')
+const app = express();
+
+const server = http.createServer(app);
+
+const wss    = new WebSocket.Server({
+    server: server
 });
 
-server.listen(8080);
+wss.on('connection', (ws: WebSocket) => {
 
-console.log('hello, world');
+    console.log('on connection in WS');
+    ws.send(`${new Date()}: Your connection was successful!`);
+
+    ws.on('message', (message: string) => {
+        console.log('on message in WS');
+        ws.send(`${new Date()}: You sent "${message}"`);
+    });
+});
+
+server.listen(HTTP_PORT, () => {
+    console.log(`${new Date()}: Server is listening on port ${server.address().port}`)
+});
